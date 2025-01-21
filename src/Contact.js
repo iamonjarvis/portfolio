0e901443e2345a50c4d66ba12e81188426266ce5
Sahil Patra <38413143+iamonjarvis@users.noreply.github.com>
2025-01-21 12:24:21 +0530

    Update resume link

--- src/Contact.js
+++ src/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
 
                     {/* New button to download resume */}
                     <div className="download-resume-button">
-                        <a href="https://drive.google.com/file/d/1T0TdHIX7LRgGXrM78dTwrZdZw_uJ-bz7/" download="Sahil_Resume" className="download-link">
+                        <a href="https://drive.google.com/file/d/1xPPSAb9ERtSgmhXbrWkQeCw9eHMymLiN/" download="Sahil_Resume" className="download-link">
                             Download Resume
                         </a>
                     </div>
